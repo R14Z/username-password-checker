31f25e68d0831e8c4fc90704b91383298808b0e3
Riaz <81258483+R14Z@users.noreply.github.com>
2021-10-12 16:21:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+origin_user = prompt('Create username ')
+origin_pass = prompt('Create password')
+
+username = prompt('Enter username ')
+password = prompt('Enter password ')
+if ((username == origin_user)) {
+  console.log('Username checked ✔')
+  // console.log('Password is ' + password)
+}
+else {
+  console.log('Username is incorrect.')
+}
+
+if ((password == origin_pass)) {
+  console.log('Password is checked ✔')
+  // console.log('Password is ' + password)
+}
+else {
+  console.log('Password is incorrect.')
+}
+
+console.log('Made with node.js : ) ')
+console.log('Check my Github account!')
+console.log('https://github.com/R14Z')
